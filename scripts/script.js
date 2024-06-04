a095ef8bc40c198f7ab5dd32e73dbfed696f36c3
Ryan Oakman <ryanoakman@gmail.com>
2024-06-04 09:20:22 -0700

    added notes to js file.

---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+"use strict";
+// get elements
+const worm = document.getElementById("snake");
+
+function directionPress() {
+  console.log(e.keyCode);
+}
+
+document.onkeydown = directionPress;
+
+// when someone presses "start", remove modal
+
+// when someone presses an arrow key, move the worm in that direction (translate x, y?)
+
+// when worm touches food object, expand worm (multiply div and add it to "back" of original)
