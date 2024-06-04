8ba88551e00f28463596d10611373ad3d03c5443
Ryan Oakman <ryanoakman@gmail.com>
2024-06-04 12:34:02 -0700

    added comments to javascript and a click event listener, added a game modal with text and a btn to index.

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,38 @@
 "use strict";
 // get elements
-const worm = document.getElementById("snake");
+// worm
+const worm = document.getElementById("worm");
+// food
+const food = document.getElementById("food");
+// play area
+const playArea = document.getElementById("game-container");
+// modal
+const newGameModal = document.getElementById("new-game-modal");
+// start game button
+const newGameBtn = document.getElementById("new-game");
+
+// when user presses play, remove modal
+newGameBtn.addEventListener("click", () => {
+  newGameModal.style.display = "none";
+});
+
+// user clicks arrow direction button, moves worm
+
+// moving worm starts an automatic move forward in that direction
+
+// clicking a different arrow key changes direction
+
+// when worm eats food, triple length of worm (add 3 divs to the backside of original div)
+
+// randomly place a new food div somewhere
+
+// contain the worm and food in the game container
+// get the game container area
+
+// count the number of divs and display it as “score”
+
+// when div 1 (head of worm) touches any part of tail
+// display game over screen and play again button
 
 function directionPress() {
   console.log(e.keyCode);
